53bdcbcace2c2aed53e4ef80e7180163f6c5eaae
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-07-05 23:26:51 +0100

    hi

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const app = express()
 const apiRouter = require('./routers/apiRouter')
-const {getToilets} = require('./controllers')
 const { handleWithCode, catchAll404, handle500 } = require('./errors/')
 app.use(express.json())
 
 app.use('/api/', apiRouter)
-app.get('/',  (req, res, next) => res.sendFile('../Front End/index.html'))
 app.use('/*', catchAll404)
 
 // app.use(handlePGerrors)
