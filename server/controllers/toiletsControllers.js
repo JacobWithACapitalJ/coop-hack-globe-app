bd8aebb4daa846a6634f0da08c4be2c8f531f51c
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-07-05 23:58:36 +0100

    CORS thingie

--- server/controllers/toiletsControllers.js
+++ server/controllers/toiletsControllers.js
@@ -3,6 +3,9 @@ const { fetchToilets } = require('../models')
 exports.getToilets = ({ queries }, res, next) => {
     fetchToilets(queries)
         .then((toilets)=> {
+            res.set({
+                ['Access-Control-Allow-Origin']: '*'
+            })
             res.status(200).send({toilets})
         })
         .catch(next)
