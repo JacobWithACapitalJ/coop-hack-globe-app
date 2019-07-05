69f758565a3e50ab3c144b001a58e8ad42f47279
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-07-05 23:41:31 +0100

    knex was in the dev dependancies

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,11 @@
 const knex = require("knex");
 const dbconfig = require("../knexfile");
-const connection = knex(dbconfig);
 
-module.exports = connection;
+const ENV = process.env.NODE_ENV || 'development';
+const config =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
+
+const connection = knex(dbconfig);
+module.exports = require('knex')(config);
