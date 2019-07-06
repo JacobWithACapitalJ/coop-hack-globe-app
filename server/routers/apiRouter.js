9d072f471ae8c3c1ab148e53d69b5e0c4059791f
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-07-06 14:43:24 +0100

    removing cores

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -4,6 +4,11 @@ const apiRouter = express.Router();
 const { getToilets } = require('../controllers')
 const { handleWithCode, catchAll404, handle500 } = require('../errors/')
 
+apiRouter.use(cors())
+
+apiRouter.get('/', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 apiRouter.use('/toilets', toiletsRouter);
 apiRouter.use('/*', catchAll404)
 apiRouter.use(handleWithCode)
