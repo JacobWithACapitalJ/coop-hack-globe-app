4be8185d2fdffb9d517fcd10149f99c14f54ef30
Jacob <jacobphillipashe@googlemail.com>
2019-07-06 14:06:10 +0100

    changes to local display

--- server/controllers/toiletsControllers.js
+++ server/controllers/toiletsControllers.js
@@ -1,14 +1,16 @@
-const { fetchToilets } = require('../models') 
-
+const { fetchToilets } = require("../models");
 
 exports.getToilets = ({ queries }, res, next) => {
-    const url = process.env.NODE_ENV === 'production' ? 'https://thunderbox.herokuapp.com/' : '*'
-    fetchToilets(queries)
-        .then((toilets)=> {
-            res.set({
-                ['Access-Control-Allow-Origin']: url
-            })
-            res.status(200).send({toilets})
-        })
-        .catch(next)
-}
+  const url =
+    process.env.NODE_ENV === "production"
+      ? "https://thunderbox.herokuapp.com/"
+      : "*";
+  fetchToilets(queries)
+    .then(toilets => {
+      res.set({
+        ["Access-Control-Allow-Origin"]: url
+      });
+      res.status(200).send({ toilets });
+    })
+    .catch(next);
+};
