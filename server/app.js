ad2b14efe4a8adc27a7ce4e40f344be6409d11fe
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-07-06 14:56:22 +0100

    make it work

--- server/app.js
+++ server/app.js
@@ -2,13 +2,7 @@ const express = require('express')
 const app = express()
 const apiRouter = require('./routers/apiRouter')
 const path = require('path')
-var cors = require('cors')
 
-app.use(cors())
-
-app.get('/', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
 
 app.use(express.static('database/toiletDatabase'))
 app.use(express.static('server/public'))
--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -2,13 +2,8 @@ const express = require('express');
 const toiletsRouter = require('./toiletsRouter')
 const apiRouter = express.Router();
 const { handleWithCode, catchAll404, handle500 } = require('../errors/')
-const cors = require('cors')
 
 apiRouter.use(cors())
-
-apiRouter.get('/', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
 apiRouter.use('/toilets', toiletsRouter);
 apiRouter.use('/*', catchAll404)
 apiRouter.use(handleWithCode)
