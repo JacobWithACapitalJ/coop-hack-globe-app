8aa689db9d745aa257eb922c701ba0b3edbc70d4
ThomasPTurner <ThomasPTurner@googlemail.com>
2019-07-06 11:45:58 +0100

    make it work

--- server/controllers/toiletsControllers.js
+++ server/controllers/toiletsControllers.js
@@ -1,10 +1,12 @@
 const { fetchToilets } = require('../models') 
 
+
 exports.getToilets = ({ queries }, res, next) => {
+    const url = process.env.NODE_ENV === 'production' ? 'https://thunderbox.herokuapp.com/' : '*'
     fetchToilets(queries)
         .then((toilets)=> {
             res.set({
-                ['Access-Control-Allow-Origin']: 'https://thunderbox.herokuapp.com/'
+                ['Access-Control-Allow-Origin']: url
             })
             res.status(200).send({toilets})
         })
